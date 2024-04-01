49a956c9cad697662063efacc4323db3267ce10f
oma <oma@pbprog.ru>
2024-04-01 12:30:58 +0300

    Initial commit

---
+++ src/main/java/org/backend/dispatcherbackend/DispatcherbackendApplication.java
@@ -0,0 +1,13 @@
+package org.backend.dispatcherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatcherbackendApplication.class, args);
+    }
+
+}
